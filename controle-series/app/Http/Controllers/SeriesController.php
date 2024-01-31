03f00c711fb5622f38cd5e5ff83d52a1f214c619
rdRafael <rafaelribeiro9436@gmail.com>
2024-01-31 19:13:25 -0300

    Transações

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -29,26 +29,30 @@ class SeriesController extends Controller
 
     public function store(SeriresFormRequest $request)
     {
-        $serie = Series::create($request->all());
-        $seasons = [];
-        for ($i = 1; $i <= $request->seasonsQty; $i++) {
-            $seasons[] = [
-                'series_id' => $serie->id,
-                'number' => $i,
-            ];
-        }
-        Season::insert($seasons);
-
-        $episodes = [];
-        foreach ($serie->season as $season) {
-            for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                $episodes[] = [
-                    'season_id' => $season->id,
-                    'number' => $j
+        $serie = DB::transaction(function () use ($request) {
+            $serie = Series::create($request->all());
+            $seasons = [];
+            for ($i = 1; $i <= $request->seasonsQty; $i++) {
+                $seasons[] = [
+                    'series_id' => $serie->id,
+                    'number' => $i,
                 ];
             }
-        }
-        Episode::insert($episodes);
+            Season::insert($seasons);
+
+            $episodes = [];
+            foreach ($serie->season as $season) {
+                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
+                    $episodes[] = [
+                        'season_id' => $season->id,
+                        'number' => $j
+                    ];
+                }
+            }
+            Episode::insert($episodes);
+
+            return $serie;
+        });
 
         return to_route('series.index')->with('mensagem.sucesso', "Série {$serie->nome} adicionada com sucesso");
     }
